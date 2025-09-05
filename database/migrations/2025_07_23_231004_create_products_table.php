1279c8c4fbf5baf13b0df9b4bc2c6a249540478b
basil832025 <1245640@gmail.com>
2025-09-05 03:22:24 +0300

    1

--- database/migrations/2025_07_23_231004_create_products_table.php
+++ database/migrations/2025_07_23_231004_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('sku')->unique();
+            $table->string('sku')->unique()->nullable();
             $table->json('title');
             $table->string('slug')->unique();
             $table->json('description')->nullable();
