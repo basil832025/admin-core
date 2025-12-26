ffad352473438ca6395e27f3fd7aee8514481fb9
basil832025 <1245640@gmail.com>
2025-12-26 21:50:41 +0200

    Update addresses redirect to point to profile addresses index

--- routes/web.php
+++ routes/web.php
@@ -232,7 +232,7 @@ Route::middleware(['web'])->group(function () {
     Route::get('/orders/history', fn() => 'Orders history stub')->name('orders.history');
     Route::get('/bonuses', fn() => 'Bonuses stub')->name('bonuses.index');
   //  Route::redirect('/profile', '/', 302)->name('profile.show');
-    Route::redirect('/addresses', '/', 302)->name('addresses.index');
+    Route::redirect('/addresses', route('profile.addresses.index'), 302)->name('addresses.index');
     // ВАЖНО: правильные имена для аутентификации
     Route::redirect('/login', '/', 302)->name('login');   // если страницы логина пока нет
     // либо так: Route::view('/login', 'pages.stub')->name('login')->defaults('title','Увійти');
