58fb36194b713ed7822183cfe95817552a3424d4
basil832025 <1245640@gmail.com>
2026-07-13 19:14:14 +0300

    Add 3piroga frontend package

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,8 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    Basil832025\FrontendThreePiroga\FrontendThreePirogaServiceProvider::class,
   //  Studio15\FilamentTree\FilamentTreeServiceProvider::class,
 ];
+
+
